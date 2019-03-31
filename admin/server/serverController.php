ac60178eb5fd6a1ffee5c4846fd43f189c3535e4
mrSmitch <rightjoint@yandex.ru>
2019-03-31 22:55:56 +0300

    initial

---
+++ admin/server/serverController.php
@@ -0,0 +1,35 @@
+<?php
+
+$formInputEnbl = "disabled";
+
+if(isset($_GET['conn']) and $_GET['conn']=='cancel'){
+    require_once($_SERVER["DOCUMENT_ROOT"]."/admin/server/views/formView.php");
+}elseif(isset($_GET['status']) and $_GET['status']=='refresh'){
+    require_once($_SERVER["DOCUMENT_ROOT"]."/admin/server/views/statusView.php");
+}elseif(isset($_POST['saveFlag']) and $_POST['saveFlag']=='y'){
+    $svContErr = null;
+    foreach($DB->connSettings as $key =>$value){
+        if(isset($_POST[$key])){
+            $DB->connSettings[$key]=$_POST[$key];
+            if($key!='CONN_PW'){
+                if($_POST[$key]==null){
+                    $svContErr[$key]="не задано";
+                }
+            }
+        }else{
+            $svContErr[$key]="отсутствует параметр";
+        }
+    }
+
+    if($svContErr!=null){
+        $formInputEnbl=null;
+    }else{
+        if(!file_put_contents($_SERVER["DOCUMENT_ROOT"].$DB->pathToConn, json_encode($DB->connSettings))){
+            $formInputEnbl['fileErr']="ошибка сохранения файла";
+        }
+    }
+    require_once($_SERVER["DOCUMENT_ROOT"]."/admin/server/views/formView.php");
+}else{
+    require_once($_SERVER["DOCUMENT_ROOT"]."/admin/server/views/defaultView.php");
+}
+
---
+++ admin/sql/views/defaultView.php
@@ -0,0 +1,28 @@
+<?php
+$appRJ->response['result'].= "<!DOCTYPE html>".
+    "<html lang='en-Us'>";
+$appRJ->response['result'].= "<head>".
+    "<meta http-equiv='content-type' content='text/html; charset=utf-8'/>".
+    "<meta name='description' content='Управление сайтом'/>".
+    "<meta name='robots' content='noindex'>".
+    "<title>".$adminModules[$adminModule]['aliasMenu']."</title>".
+    "<link rel='SHORTCUT ICON' href='/admin/img/favicon.png' type='image/png'>".
+    "<script src='/source/js/jquery-3.2.1.js'></script>".
+    "<link rel='stylesheet' href='/admin/css/default.css' type='text/css' media='screen, projection'/>".
+    "<link rel='stylesheet' href='/admin/adminHeader/css/adminHeader.css' type='text/css' media='screen, projection'/>".
+    "<script src='/admin/adminHeader/js/adminHeader.js'></script>".
+    "<link rel='stylesheet' href='/admin/sql/css/default.css' type='text/css' media='screen, projection'/>".
+    "<script src='/admin/sql/js/default.js'></script>".
+    "<link rel='stylesheet' href='/source/js/Elegant-Loading-Indicator-jQuery-Preloader/src/css/preloader.css'/>".
+    "<script src='/source/js/Elegant-Loading-Indicator-jQuery-Preloader/src/js/jquery.preloader.min.js'></script>".
+    "</head><body>";
+require_once($_SERVER["DOCUMENT_ROOT"] . "/admin/adminHeader/views/adminHeader.php");
+$appRJ->response['result'].= "<div class='contentBlock-frame'><div class='contentBlock-center'>".
+    "<div class='contentBlock-wrap'>".
+    "<form><label for='tagretQuery'>Введите запрос:</label><textarea name='tagretQuery' rows='5'></textarea></form>".
+    "<div class='queryPanel'><div class='queryPanel-left'><span class='resTxt'>Результат: </span>".
+    "<div class='queryResults'>-</div></div><div class='queryPanel-right'>".
+    "<input type='button' value='mkQuery' onclick='mkQuery()'></div></div>";
+$appRJ->response['result'].= "</div></div></div>";
+require_once($_SERVER["DOCUMENT_ROOT"] . "/admin/adminFooter/views/footerDefault.php");
+$appRJ->response['result'].= "</body></html>";
